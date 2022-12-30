17d2e6c56a3eb442bdd6cc538719ac1e1e155750
DubiNir <121538379+DubiNir@users.noreply.github.com>
2022-12-30 22:19:22 +0200

    Add files via upload

---
+++ mail/contact.js
@@ -0,0 +1,98 @@
+$(function () {
+
+    $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function ($form, event, errors) {
+        },
+        submitSuccess: function ($form, event) {
+            event.preventDefault();
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var subject = $("input#subject").val();
+            var message = $("textarea#message").val();
+
+            $this = $("#sendMessageButton");
+            $this.prop("disabled", true);
+
+            $.ajax({
+                url: "contact.php",
+                type: "POST",
+                data: {
+                    name: name,
+                    email: email,
+                    subject: subject,
+                    message: message
+                },
+                cache: false,
+                success: function () {
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                    $('#success > .alert-success')
+                            .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                            .append('</div>');
+                    $('#contactForm').trigger("reset");
+                },
+                error: function () {
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
+                    $('#success > .alert-danger').append('</div>');
+                    $('#contactForm').trigger("reset");
+                },
+                complete: function () {
+                    setTimeout(function () {
+                        $this.prop("disabled", false);
+                    }, 1000);
+                }
+            });
+        },
+        filter: function () {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function (e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+$('#name').focus(function () {
+    $('#success').html('');
+});
+
+<script>
+	var form = document.getElementById("my-form");
+									
+	async function handleSubmit(event) {
+		event.preventDefault();
+		var status = document.getElementById("my-form-status");
+		var data = new FormData(event.target);
+		fetch(event.target.action, {
+		method: form.method,
+		body: data,
+		headers: {
+		'Accept': 'application/json'
+		}
+		}).then(response => {
+			if (response.ok) {
+			status.innerHTML = "Thanks for your submission!";
+			form.reset()
+		} else {
+			response.json().then(data => {
+				if (Object.hasOwn(data, 'errors')) {
+					status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+					} else {
+					status.innerHTML = "Oops! There was a problem submitting your form"
+					}
+					})
+					}
+					}).catch(error => {
+					status.innerHTML = "Oops! There was a problem submitting your form"
+					});
+					}
+					form.addEventListener("submit", handleSubmit)
+</script>
